c5f3c1d6cfd872ad6c8cd3d0a26768a2809e8310
Karabo Makganyoga <kamakjhb025@student.wethinkcode.co.za>
2026-01-16 12:42:48 +0200

    made Quiz-Brain and Questions classes

---
+++ quiz_brain.py
@@ -0,0 +1,12 @@
+
+
+
+class QuizBrain:
+    def __init__(self,question_list):
+        self.question_number = 0
+        self.question_list = question_list
+
+
+    def next_question(self):
+        question = self.question_list[self.question_number]
+        input(f"Q. {self.question_number}: {question.text}? (True/False)")
